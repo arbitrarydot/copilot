65abd91d9cb89546e51fa69b2f51782a1b1b2596
JaxonWright <jaxonhwright@gmail.com>
2019-12-10 22:35:46 -0500

    user-service: added interface

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -1,4 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Car } from '../car/car.service';
+
+
+
+export interface User {
+  id : string,
+  email : string,
+  firstName : string,
+  lastName : string,
+  premium : boolean,
+  founder : boolean,
+  cars : Car[],
+  token : string,
+}
+
+
 
 @Injectable({
   providedIn: 'root'
