8d10422e913194fe13bb9d9fa23dca19302118df
JaxonWright <jaxonhwright@gmail.com>
2019-12-16 21:19:24 -0500

    Routing: added "dash" path

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AuthenticateComponent } from './pages/authenticate/authenticate.component';
+import { DashComponent } from './pages/dash/dash.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'auth', pathMatch: 'full'},
   { path: 'auth', component: AuthenticateComponent },
+  { path: 'dash', component: DashComponent },
   { path: '**', redirectTo: 'auth' }
 ];
 
