edf460b4db75c0919675fd62da5965d7636a1863
JaxonWright <jaxonhwright@gmail.com>
2019-12-16 21:18:39 -0500

    Create dash component for main dashboard

---
+++ src/app/pages/dash/dash.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { TeslaApiService } from 'src/app/services/tesla-api/tesla-api.service';
+import { CarService } from 'src/app/services/car/car.service';
+
+@Component({
+  selector: 'app-dash',
+  templateUrl: './dash.component.html',
+  styleUrls: ['./dash.component.scss']
+})
+export class DashComponent implements OnInit {
+  public vehicles;
+  public selectedVehicle : number;
+
+  constructor(private tesla : TeslaApiService,
+              public  car   : CarService) { }
+
+  ngOnInit() {
+    this.getVehicles();
+  }
+
+  getVehicles() {
+    this.tesla.getVehicles().subscribe((res : any) => {
+      this.vehicles = res.response;
+      if (this.vehicles.length = 1) {
+        this.selectedVehicle = this.vehicles[0].vehicle_id;
+        this.car.setId(this.selectedVehicle);
+      }
+    })
+  }
+
+}
